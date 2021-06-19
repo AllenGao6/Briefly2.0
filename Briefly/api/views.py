2cc3bca9807ef6783b766f44588b652125b4329f
Lucheng_Hugh <hughqing@qq.com>
2021-06-19 14:42:59 +0800

    experimentally deploying summary API

--- Briefly/api/views.py
+++ Briefly/api/views.py
@@ -1,4 +1,5 @@
 from django.db.models.expressions import Col
+from django.db.models.fields import NullBooleanField
 from .models import Collection, Video
 from django.shortcuts import render
 from rest_framework import serializers, viewsets
@@ -9,11 +10,12 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework.authentication import SessionAuthentication, TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from Briefly import settings
 import boto3
 from .permissions import CollectionUserPermission,VideoUserPermission
 from django.db.models import Q
+from django.core.mail import send_mail
 
 class VideoViewSet(viewsets.ModelViewSet):
 
@@ -25,15 +27,13 @@ class VideoViewSet(viewsets.ModelViewSet):
         
         return Video.objects.filter(Q(collection=self.kwargs['collection_pk']) & Q(collection__owner=user))
 
-    '''similar to post_save: call save twice to know the id of the video just created and save to the correct directory'''
+    '''
+    similar to post_save: call save twice to know the id of the video just created and save to the correct directory
+    '''
     def perform_create(self, serializer):
         instance = serializer.save()
         instance.video.delete(save=False)
         serializer.save()  #update from serializer, worked
-    
-    def perform_destroy(self, instance):
-        instance.video.delete(save=False)
-        return super().perform_destroy(instance)
         
         '''another way: update from instance itself, also worked but lengthy'''
         # update from instance itself
@@ -47,20 +47,61 @@ class VideoViewSet(viewsets.ModelViewSet):
         ''' No need to do this step: but keep here as a reference'''
         #s3 = boto3.resource('s3')
         #s3.Object(settings.AWS_STORAGE_BUCKET_NAME, url).delete()
-
+    
+    def perform_destroy(self, instance):
+        instance.video.delete(save=False)
+        return super().perform_destroy(instance)
         
+      
     #override create
     '''def create(self, request, *args, **kwargs):
         print(self.kwargs['collection_pk'])
         return Response({})
     '''
-    # Test
-    @action(methods=['get'], detail=False)
-    def newest(self, request):
-        newest = self.get_queryset().order_by("created").last()
-        serializer = self.get_serializer_class()(newest)
-        return Response(serializer.data)
-
+    
+    # # Test
+    # @action(methods=['get'], detail=False)
+    # def newest(self, request):
+    #     newest = self.get_queryset().order_by("created").last()
+    #     serializer = self.get_serializer_class()(newest)
+    #     return Response(serializer.data)
+    
+    '''
+    Description: when summary is ready, this should be called to send email
+    '''
+    def summary_ready(self, video):
+        mail = send_mail(
+            'You Summarization Is Ready at Briefly-AI',
+            f'Hi {video.collection.owner},\n      your video: {video.title} in {video.collection} is ready!\n      please go to Briefly-AI.com to check out!',
+            'hugh_megumi@qq.com',
+            [str(video.collection.owner.email)],
+            fail_silently=False
+        )
+        return mail
+    
+    '''
+    Description: call this url to begin summarization
+    URL: summary_begin (always the method name)
+    '''
+    @action(methods=['get'],detail=True)
+    def summary_begin(self, request, *args, **kwargs):
+        user = request.user
+        video = Video.objects.filter(Q(pk=self.kwargs['pk']) & Q(collection__owner=user))
+        if video:
+            video = video[0]
+            
+            if not video.summarization:
+                try:
+                    # code to perform summarization process
+                    self.summary_ready(video)
+                    return Response("Success!")
+                except:
+                    return Response("fail to summarize", status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    
+    
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def upload_video(request):
@@ -82,3 +123,4 @@ class CollectionViewSet(viewsets.ModelViewSet):
         instance = serializer.save()
         instance.image.delete(save=False)
         serializer.save()  #update from serializer, worked
+
