458fe169061e2b5ae84660e8d451d10157ec203e
Lucheng_Hugh <hughqing@qq.com>
2021-08-06 15:41:25 +0800

    set to production setting

--- Briefly/Briefly/settings.py
+++ Briefly/Briefly/settings.py
@@ -207,13 +207,13 @@ AWS_S3_FILE_OVERWRITE = False
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-#EMAIL_PORT = 587
-#EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_SSL = False
 EMAIL_USE_TLS = True
 
-EMAIL_HOST = 'smtp.qq.com'
-EMAIL_PORT = 25
+#EMAIL_HOST = 'smtp.qq.com'
+#EMAIL_PORT = 25
 
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
