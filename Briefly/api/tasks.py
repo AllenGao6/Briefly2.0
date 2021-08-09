066e4d5521bbe430cf67cd8588cd10af2efa3ff5
Lucheng_Hugh <hughqing@qq.com>
2021-08-09 16:29:05 +0800

    Update tasks.py

--- Briefly/api/tasks.py
+++ Briefly/api/tasks.py
@@ -136,12 +136,14 @@ def XLNet_summarize_celery(tuple_args, num_sentence=None, max_sentence = 20):
 
 '''This functionality has been removed to another server on another instance'''
 @shared_task(base=QuizTask, time_limit=600)
-def pop_quiz_celery(tuple_args, kwargs):
+def pop_quiz_celery(*tuple_args, **kwargs):
     print('starting process quiz...')
     summary, audioText, video_info = tuple_args
+    
     based_text = kwargs.get('based_text',"summ")
     type_task = kwargs.get('type_task', 'QA_pair_gen')
     question = kwargs.get('question', None)
+    
     Quiz = Quiz_generation(summary, audioText, based_text=based_text, model = pop_quiz_celery.model)
     res = Quiz.generate(type_task, question=question)
 
